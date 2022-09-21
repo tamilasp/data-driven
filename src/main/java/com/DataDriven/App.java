2ad893e06ce1d26c35885c84f16e9a42d243c443
tamilasp <tamiltwisters98@gmail.com>
2022-09-21 18:39:14 +0700

    initial commit

---
+++ src/main/java/com/DataDriven/App.java
@@ -0,0 +1,13 @@
+package com.DataDriven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
